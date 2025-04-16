c1c5d24bba14cbcd8ea35e4dca7ae2a364d0c8f6
Raj Narayan <hazrarajnarayan55@gmail.com>
2025-04-16 17:02:34 +0530

    Update exppp01.ino

--- Home automation app/esp/exppp01.ino
+++ Home automation app/esp/exppp01.ino
@@ -1,42 +1,73 @@
 #include <ESP8266WiFi.h>
 
+
+
 WiFiClient client;
+
 WiFiServer server(80);
 
 
+
 void setup() {
-  Serial.begin(9600); // To Arduino via TX pin
+
+  Serial.begin(9600);
+
   WiFi.begin("DESKTOP-IOT", "12345678");
 
+
+
   while (WiFi.status() != WL_CONNECTED) {
+
     delay(200);
-    Serial.print("..");
+
+    Serial.print(".");
+
   }
 
-  Serial.println();
-  Serial.println("NodeMCU is connected!");
+
+
+  Serial.println("\nConnected to WiFi");
+
   Serial.println(WiFi.localIP());
 
   server.begin();
 
-
 }
+
+
 
 void loop() {
-  client = server.available(); // Check if a client is connected
+
+  client = server.available();
+
+
 
   if (client) {
+
     String request = client.readStringUntil('\r');
-    Serial.print(request);  // 🔁 Send to Arduino
+
+    Serial.print(request);
 
 
 
-    // Send a basic HTTP response
+   
+
+
     client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/html");
-    client.println("Connection: close");
-    client.println();
-    client.println("<html><body><h1>ESP8266 Received</h1></body></html>");
-    client.stop(); // Close the connection
-  }
-}
+client.println("Content-Type: text/html");
+client.println("Connection: close");
+client.println();
+
+client.println("<!DOCTYPE html><html><head><title>Home Automation</title>");
+client.println("<style>body{background:#000;color:#fff;text-align:center;font-family:sans-serif}.button{display:inline-block;padding:12px 20px;margin:10px;background:#4CAF50;color:white;text-decoration:none;border-radius:6px}</style>");
+client.println("</head><body><h1>ESP8266 Home Control</h1>");
+client.println("<a class='button' href='/led1on'>Switch 1 ON</a>");
+client.println("<a class='button' href='/led1off'>Switch 1 OFF</a>");
+client.println("<a class='button' href='/led2on'>Switch 2 ON</a>");
+client.println("<a class='button' href='/led2off'>Switch 2 OFF</a>");
+client.println("<a class='button' href='/on'>ALL ON</a>");
+client.println("<a class='button' href='/off'>ALL OFF</a>");
+client.println("</body></html>");
+
+client.stop();
+  }}
