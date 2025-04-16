24b19adc7025e0a366557c66adfe03068881fff5
Raj Narayan <hazrarajnarayan55@gmail.com>
2025-04-16 17:08:36 +0530

    Update and rename main.c to .c

--- Home automation app/arduino/.c
+++ Home automation app/arduino/.c
@@ -12,38 +12,38 @@ void loop() {
   { Serial.println();
     String val = Serial.readStringUntil('\n');  //Read the data recieved
 
-    if (val == "GET/led1on HTTP/1.1") {
+    if (val == "GET /led1on HTTP/1.1") {
       digitalWrite(8, HIGH);
       Serial.print("Received data:");
       Serial.println(val);
       Serial.println("Switch 1 is ON");
     }
-    if (val == "GET/led1off HTTP/1.1") {
+    if (val == "GET /led1off HTTP/1.1") {
       digitalWrite(8, LOW);
       Serial.print("Received data:");
       Serial.println(val);
       Serial.println("Switch 1 is OFF");
     }
-    if (val == "GET/led2on HTTP/1.1") {
+    if (val == "GET /led2on HTTP/1.1") {
       digitalWrite(9, HIGH);
       Serial.print("Received data:");
       Serial.println(val);
       Serial.println("Switch 2 is ON");
     }
-    if (val == "GET/led2off HTTP/1.1") {
+    if (val == "GET /led2off HTTP/1.1") {
       digitalWrite(9, LOW);
       Serial.print("Received data:");
       Serial.println(val);
       Serial.println("Switch 2 is OFF");
     }
-    if (val == "GET/on HTTP/1.1") {
+    if (val == "GET /on HTTP/1.1") {
       digitalWrite(8, HIGH);
       digitalWrite(9, HIGH);
       Serial.print("Received data:");
       Serial.println(val);
       Serial.println("BOTH SWITCHES ON");
     }
-    if (val == "GET/off HTTP/1.1") {
+    if (val == "GET /off HTTP/1.1") {
       digitalWrite(8, LOW);
       digitalWrite(9, LOW);
       Serial.print("Received data:");
