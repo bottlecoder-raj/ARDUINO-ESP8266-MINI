9a49607d7e4cac13102aed04ff9cd9bd1d6406ab
Raj Narayan <hazrarajnarayan55@gmail.com>
2025-04-16 14:15:08 +0530

    Update main.c

--- Home automation app/arduino/main.c
+++ Home automation app/arduino/main.c
@@ -9,34 +9,45 @@ void setup() {
 
 void loop() {
   if (Serial.available() != 0)  //if communication is going on
-  {
+  { Serial.println();
     char val = Serial.read();  //Read the data recieved
-    Serial.print("Received data:");
-    Serial.println(val);
+
     if (val == 'A') {
       digitalWrite(8, HIGH);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("Switch 1 is ON");
     }
     if (val == 'a') {
       digitalWrite(8, LOW);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("Switch 1 is OFF");
     }
     if (val == 'B') {
       digitalWrite(9, HIGH);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("Switch 2 is ON");
     }
     if (val == 'b') {
       digitalWrite(9, LOW);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("Switch 2 is OFF");
     }
-     if (val == 'C'){
-    digitalWrite(8, HIGH);
+    if (val == 'C') {
+      digitalWrite(8, HIGH);
       digitalWrite(9, HIGH);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("BOTH SWITCHES ON");
     }
-    if (val == 'C'){
-    digitalWrite(8, LOW);
+    if (val == 'c') {
+      digitalWrite(8, LOW);
       digitalWrite(9, LOW);
+      Serial.print("Received data:");
+      Serial.println(val);
       Serial.println("BOTH SWITCHES OFF");
     }
   }
