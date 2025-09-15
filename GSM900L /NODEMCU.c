1a132f39dcad84692c812a31083176576f8fdbc5
Raj Narayan <hazrarajnarayan55@gmail.com>
2025-09-15 22:18:41 +0530

    Update NODEMCU.c

--- GSM900L /NODEMCU.c
+++ GSM900L /NODEMCU.c
@@ -1,19 +1,24 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+bool motor_status = true;
+int sms_stat=0;
 
 // NodeMCU pins for SIM900L
 SoftwareSerial sim900(D5, D6);  // RX, TX
 
-#define LED_PIN D4   // GPIO14 - LED connected here
+#define LED_PIN D0   // GPIO14 - LED connected here
 
-String senderNumber = "+918169456269";
+String senderNumber = "+919702789546";
 
 void setup() {
   Serial.begin(115200);
   sim900.begin(9600);
+  myservo.attach(D3);
+  myservo.write(0);
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_PIN, HIGH);
 
-  delay(2000);
   Serial.println("Initializing SIM900L...");
   Serial.println(" SIM900L activating...");
 
@@ -37,34 +42,37 @@ void loop() {
 
     sms.toUpperCase(); // make comparison case-insensitive
 
-    if (sms.indexOf("LED ON") >= 0) {
-      digitalWrite(LED_PIN, HIGH);
+    if (sms.indexOf("ON") >= 0) {
       delay(500);
+      sms_stat+=1;
+       Serial.println("IRRIGATION Turned ON");
+      if(sms_stat==1){
+      sendSMS(senderNumber, "MOTOR is now ON");
+      } sms_stat=0;
+      Serial.print("STATUS:");
+        Serial.println(sms_stat); //motorOn();
       digitalWrite(LED_PIN, LOW);
-            delay(500);
-         digitalWrite(LED_PIN, HIGH);
-               delay(500);
-      digitalWrite(LED_PIN, LOW);
-            delay(500);
-         digitalWrite(LED_PIN, HIGH);
-               delay(500);
-      digitalWrite(LED_PIN, LOW);
-            delay(500);
-      Serial.println("LED Turned ON");
-      sendSMS(senderNumber, "LED is now ON");
-    }
-    else if (sms.indexOf("LED OFF") >= 0) {
+        myservo.write(180);
+        delay(500);
+     }
+    else if (sms.indexOf("OFF") >= 0) {
+      sms_stat+=2;
+      Serial.println("IRRIGATION Turned OFF");
+      if(sms_stat==2){
+      sendSMS(senderNumber, "MOTOR is now OFF");}
+      sms_stat=0;
       digitalWrite(LED_PIN, HIGH);
-      Serial.println("LED Turned OFF");
-      sendSMS(senderNumber, "LED is now OFF");
-    }
+      Serial.print("STATUS:");
+      Serial.println(sms_stat); 
+      myservo.write(0);
+        delay(500);    }
   }
 }
 
 // Function to send SMS
 void sendSMS(String number, String text) {
   if (number.length() == 0) {
-    number = "+918169456269"; // fallback: put your number here
+    number = "+919702789546"; // fallback: put your number here
   }
 
   sim900.println("AT+CMGF=1"); // text mode
